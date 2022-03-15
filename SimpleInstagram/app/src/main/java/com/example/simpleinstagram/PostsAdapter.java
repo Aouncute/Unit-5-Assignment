2bf25a8cdafe09521f954d7b8ff83a5e8a382c94
Aouncute <amornratajmo@gmail.com>
2022-03-15 16:38:22 -0400

    Latest update

--- SimpleInstagram/app/src/main/java/com/example/simpleinstagram/PostsAdapter.java
+++ SimpleInstagram/app/src/main/java/com/example/simpleinstagram/PostsAdapter.java
@@ -44,23 +44,38 @@ public class PostsAdapter  extends RecyclerView.Adapter<PostsAdapter.ViewHolder>
         return posts.size();
     }
 
+    // Clean all elements of the recycler
+    public void clear(){
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> postList) {
+        posts.addAll(postList);
+        notifyDataSetChanged();
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder{
 
         private TextView tvUsername;
         private ImageView ivImage;
         private TextView tvDescription;
+        private TextView tvCreatedAt;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUsername = itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvCreatedAt = itemView.findViewById(R.id.tvCreatedAt);
         }
 
         public void bind(Post post) {
             // Bind the post data to the view elements
             tvDescription.setText(post.getDescription());
             tvUsername.setText(post.getUser().getUsername());
+            tvCreatedAt.setText(post.getCreatedAt().toString());
             ParseFile image = post.getImage();
             if (image != null) {
                 Glide.with(context).load(image.getUrl()).into(ivImage);
